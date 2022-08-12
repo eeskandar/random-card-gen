1005d7dead89b9ba33dd131ad7716fef50aa0ec4
Iskandar <seik747@gmail.com>
2022-08-12 15:43:12 +0000

    estableciendo la funcionalidad para la card gen

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomStuff(max) {
+  return Math.floor(Math.random() * max);
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //bringing the elements from HTML
+  const signTop = document.querySelector(".sign-top");
+  const cardText = document.querySelector(".card-t");
+  const signBot = document.querySelector(".sign-bot");
+
+  //defining values
+  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let pinta = ["♦", "♥", "♠", "♣"];
+
+  //doing the random stuff
+  let blackOrRed = randomStuff(4);
+  let numberRandom = number[randomStuff(number.length)];
+  let pintaRandom = pinta[blackOrRed];
+
+  //to know if the sign should be red
+  if (blackOrRed == 0 || blackOrRed == 1) {
+    signTop.classList.add("red");
+    signBot.classList.add("red");
+  }
+
+  //values to display
+  signTop.textContent = pintaRandom;
+  cardText.textContent = numberRandom;
+  signBot.textContent = pintaRandom;
 };
